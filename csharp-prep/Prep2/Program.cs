644ea521f4c0610a36aff711d57e7fc57e2a5532
15claytmatr <126034428+15claytmatr@users.noreply.github.com>
2023-02-27 11:43:24 -0700

    Update Program.cs

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,47 @@
 using System;
 
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What grad percentage do you have in class?");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+         }
+        
+        else if (percent >= 80)
+        {
+         letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+            Console.WriteLine("Your letter grade is: {letter}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("You have a passing grade! Congrats!");
+        }
+        else
+        {
+            Console.WriteLine ("you have not passed this class");
+        }
     }
 }
