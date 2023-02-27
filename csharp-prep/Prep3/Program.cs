0b298630b8de3812c3980319dbbf8f36bca802ca
15claytmatr <126034428+15claytmatr@users.noreply.github.com>
2023-02-27 13:29:09 -0700

    Update Program.cs

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,33 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
+class program
+
+static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        Random randomGenerator = new Random();
+        int correctnumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != correctnumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (correctnumber > guess)
+            {
+                Console.WriteLine("Number is higher");
+            }
+            else if (correctnumber < guess)
+            {
+                Console.WriteLine("Number is lower");
+            }
+            else
+            {
+                Console.WriteLine("Correct!");
+            }
+
+        }                    
     }
 }
