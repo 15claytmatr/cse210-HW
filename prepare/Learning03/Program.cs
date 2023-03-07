d4601d6810e420e6a1e1fa743e678b56e98f2276
15claytmatr <126034428+15claytmatr@users.noreply.github.com>
2023-03-07 12:16:17 -0700

    Update Program.cs

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,50 @@
 using System;
 
-class Program
+public class Fraction
 {
-    static void Main(string[] args)
+    private readonly int _numerator;
+    private readonly int _denominator;
+
+    public Fraction() : this(1, 1)
     {
-        Console.WriteLine("Hello Learning03 World!");
+    }
+
+    public Fraction(int wholeNumber) : this(wholeNumber, 1)
+    {
+    }
+
+    public Fraction(int numerator, int denominator)
+    {
+        if (denominator == 0)
+        {
+            throw new ArgumentException("Denominator cannot be zero.");
+        }
+
+        int gcd = Gcd(numerator, denominator);
+
+        _numerator = numerator / gcd;
+        _denominator = denominator / gcd;
+    }
+
+    public string GetFractionString()
+    {
+        return $"{_numerator}/{_denominator}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_numerator / _denominator;
+    }
+
+    private static int Gcd(int a, int b)
+    {
+        while (b != 0)
+        {
+            int remainder = a % b;
+            a = b;
+            b = remainder;
+        }
+
+        return a;
     }
 }
